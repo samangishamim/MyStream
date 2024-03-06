9e7c61388ae4e7524e5f5fe53cc4d27f35406e4d
samangishamim <samangi624@gmail.com>
2024-03-07 02:35:49 +0330

    add filterPeopleByAge method in main for calculate

--- src/main/java/ir/maktab/Main.java
+++ src/main/java/ir/maktab/Main.java
@@ -1,9 +1,23 @@
 package ir.maktab;
 
 
+import ir.maktab.mockdata.MockData;
+import ir.maktab.model.Person;
+import ir.maktab.model.PersonSummary;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    private static List<Person> filterPeopleByAge(List<Person> people) {
+        return people.stream()
+                .filter(person -> person.getAge() <= 50)
+                .collect(Collectors.toList());
+    }
+
+
 }
