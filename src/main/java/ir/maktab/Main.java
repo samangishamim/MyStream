92466297aaea5a33068c560bb6f3fee8e3c00706
samangishamim <samangi624@gmail.com>
2024-03-07 02:26:04 +0330

    open the file in intellij

---
+++ src/main/java/ir/maktab/Main.java
@@ -0,0 +1,9 @@
+package ir.maktab;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ir/maktab/model/Person.java
@@ -0,0 +1,24 @@
+package ir.maktab.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Person {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private String gender;
+    private String email;
+    private String phone;
+    private String username;
+    private String favoriteColor;
+    private String birthDate;
+    private String ipv4;
+    private String ipv6;
+
+
+}
